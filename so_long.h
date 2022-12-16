8017a7e201e261b32e83271c993c318ea330946c
ngennaro <ngennaro@z1r12p1.42lyon.fr>
2022-12-16 13:09:18 +0100

    re structure libraries

--- so_long.h
+++ so_long.h
@@ -1,28 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngennaro <ngennaro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/08 15:50:21 by ngennaro          #+#    #+#             */
-/*   Updated: 2022/12/14 14:34:37 by ngennaro         ###   ########lyon.fr   */
+/*   Created: 2022/12/16 11:10:54 by ngennaro          #+#    #+#             */
+/*   Updated: 2022/12/16 11:39:20 by ngennaro         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef SO_LONG_H
+# define SO_LONG_H
 
-# include <unistd.h>
-# include <stdlib.h>
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
-# endif
-
-char	*get_next_line(int fd);
-
-int		is_end_line(const char *str, size_t *index);
-char	*join(char const *s1, char const *s2);
-void	buffer_clear(char *str, size_t len);
+# include "libft.h"
+# include "ft_printf.h"
+# include "get_next_line.h"
 
 #endif
