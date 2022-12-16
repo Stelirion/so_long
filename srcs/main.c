38b61de282b4f7d3a3be8897fb0099db4135e7a3
ngennaro <ngennaro@z1r12p1.42lyon.fr>
2022-12-16 17:04:07 +0100

    working all makefile

--- srcs/main.c
+++ srcs/main.c
@@ -1,21 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngennaro <ngennaro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/16 11:10:54 by ngennaro          #+#    #+#             */
-/*   Updated: 2022/12/16 17:01:16 by ngennaro         ###   ########lyon.fr   */
+/*   Created: 2022/12/16 16:58:24 by ngennaro          #+#    #+#             */
+/*   Updated: 2022/12/16 16:58:28 by ngennaro         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
+#include "so_long.h"
 
-# include "libft.h"
-# include "ft_printf.h"
-# include "get_next_line_bonus.h"
-# include "./mlx/mlx.h"
-
-#endif
+int	main(void)
+{
+	ft_printf("salut\n");
+}
