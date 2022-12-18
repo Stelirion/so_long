eb5ebd144b90dc9068553bc01872a37ba5e9e8a6
ngennaro <ngennaro@z1r12p1.42lyon.fr>
2022-12-18 16:06:46 +0100

    update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ngennaro <ngennaro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 16:58:24 by ngennaro          #+#    #+#             */
-/*   Updated: 2022/12/18 16:01:01 by ngennaro         ###   ########lyon.fr   */
+/*   Updated: 2022/12/18 16:06:38 by ngennaro         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,6 @@ int	main(int argc, char **argv)
 		return (ft_printf("ERREUR, Nombre d'arguments invalide\n"));
 	maps = read_maps(argv[1]);
 	if (!maps)
-		return(ft_printf("ERREUR, Map invalide"));
+		return (ft_printf("ERREUR, Map invalide"));
 	print_maps(maps);
 }
