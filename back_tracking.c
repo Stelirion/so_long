0f9b88258e469271d82a054ae0f314663ab67964
ngennaro <ngennaro@z1r12p1.42lyon.fr>
2023-01-09 14:02:34 +0100

    norme

--- back_tracking.c
+++ back_tracking.c
@@ -6,7 +6,7 @@
 /*   By: ngennaro <ngennaro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 14:33:53 by ngennaro          #+#    #+#             */
-/*   Updated: 2023/01/06 16:42:01 by ngennaro         ###   ########lyon.fr   */
+/*   Updated: 2023/01/09 14:01:48 by ngennaro         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,18 +32,70 @@ char	**map_dup(char **maps)
 	return (temp_maps);
 }
 
-int	check_faisability(t_mlx *mlx)
+int	final_check(char **temp_maps)
 {
-	char	**temp_maps;
+	int	x;
+	int	y;
+	int	exit;
+
+	x = 0;
+	while (temp_maps[x])
+	{
+		y = 0;
+		while (temp_maps[x][y])
+		{
+			if (temp_maps[x][y] == 'C')
+				return (1);
+			if (temp_maps[x][y] == 'E')
+			{
+				if (temp_maps[x + 1][y] == '*' || temp_maps[x - 1][y] == '*'
+					|| temp_maps[x][y + 1] == '*' || temp_maps[x][y - 1] == '*')
+					exit = 1;
+				else
+					return (1);
+			}
+			y++;
+		}
+		x++;
+	}
+	return (0);
+}
+
+void	replace_chr(char **temp_maps, int x, int y, int *new)
+{
+	if (temp_maps[x + 1][y] == '0' ||
+		temp_maps[x + 1][y] == 'C')
+	{
+		*new = 1;
+		temp_maps[x + 1][y] = '*';
+	}
+	if (temp_maps[x - 1][y] == '0' ||
+		temp_maps[x - 1][y] == 'C')
+	{
+		*new = 1;
+		temp_maps[x - 1][y] = '*';
+	}
+	if (temp_maps[x][y + 1] == '0' ||
+		temp_maps[x][y + 1] == 'C')
+	{
+		*new = 1;
+		temp_maps[x][y + 1] = '*';
+	}
+	if (temp_maps[x][y - 1] == '0' ||
+		temp_maps[x][y - 1] == 'C')
+	{
+		*new = 1;
+		temp_maps[x][y - 1] = '*';
+	}
+}
+
+void	parse_all(char **temp_maps)
+{
 	int		x;
 	int		y;
 	int		new;
 
-	temp_maps = map_dup(mlx->maps);
-	if (!temp_maps)
-		return (1);
 	new = 1;
-	temp_maps[mlx->player_y][mlx->player_x] = '*';
 	while (new != 0)
 	{
 		x = 0;
@@ -55,62 +107,25 @@ int	check_faisability(t_mlx *mlx)
 			{
 				if (temp_maps[x][y] == '*')
 				{
-					if (temp_maps[x + 1][y] == '0' ||
-						temp_maps[x + 1][y] == 'C')
-					{
-						new = 1;
-						temp_maps[x + 1][y] = '*';
-					}
-					if (temp_maps[x - 1][y] == '0' ||
-						temp_maps[x - 1][y] == 'C')
-					{
-						new = 1;
-						temp_maps[x - 1][y] = '*';
-					}
-					if (temp_maps[x][y + 1] == '0' ||
-						temp_maps[x][y + 1] == 'C')
-					{
-						new = 1;
-						temp_maps[x][y + 1] = '*';
-					}
-					if (temp_maps[x][y - 1] == '0' ||
-						temp_maps[x][y - 1] == 'C')
-					{
-						new = 1;
-						temp_maps[x][y - 1] = '*';
-					}
+					replace_chr(temp_maps, x, y, &new);
 				}
 				y++;
 			}
 			x++;
 		}
 	}
-	x = 0;
-	while (temp_maps[x])
-	{
-		y = 0;
-		while (temp_maps[x][y])
-		{
-			if (temp_maps[x][y] == 'C')
-				return (free_maps(temp_maps), 1);
-			if (temp_maps[x][y] == 'E')
-			{
-				if (temp_maps[x + 1][y] == '*')
-					new = 1;
-				else if (temp_maps[x - 1][y] == '*')
-					new = 1;
-				else if (temp_maps[x][y + 1] == '*')
-					new = 1;
-				else if (temp_maps[x][y - 1] == '*')
-					new = 1;
-				else
-					return (free_maps(temp_maps), 1);
-
-			}
-			y++;
-		}
-		x++;
-	}
-	return (free_maps(temp_maps), 0);
 }
 
+int	check_faisability(t_mlx *mlx)
+{
+	char	**temp_maps;
+
+	temp_maps = map_dup(mlx->maps);
+	if (!temp_maps)
+		return (1);
+	temp_maps[mlx->player_y][mlx->player_x] = '*';
+	parse_all(temp_maps);
+	if (final_check(temp_maps) == 1)
+		return (free_maps(temp_maps), 1);
+	return (free_maps(temp_maps), 0);
+}
